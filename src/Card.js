cc55ebbf33259377f18e6085703f96246d3b8cd8
Ray <ray644302280@gmail.com>
2020-07-29 12:15:34 +1000

    Add Robot Card
    - tachyons
    - RoboHash

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = ({id,name,email}) => {
+    return (
+        <div className="tc bg-light-green dib br3 ma2 pa3 grow shadow-5">
+            <img alt="robot_image" src={`https://robohash.org/${id}?size=200x200`}></img>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import {robots} from './robot';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <div>
+    {robots.map(robot=><Card id={robot.id} name={robot.name} email={robot.email}/>)}
+  </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
