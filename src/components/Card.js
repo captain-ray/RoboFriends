c1e6c523b7111e2d1cbefcb75666a6be63a1343c
Ray <ray644302280@gmail.com>
2020-07-31 11:06:03 +1000

    snapshot testing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 //pure component
-const Card = ({id,name,email}) => {
+const Card = ({ id, name, email }) => {
     return (
-        <div className="tc bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
+        <div className=" bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
             <img alt="robot_image" src={`https://robohash.org/${id}?size=200x200`}></img>
             <h2>{name}</h2>
             <p>{email}</p>
