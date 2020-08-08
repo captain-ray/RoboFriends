d70f30542fe15d5557f0190fba71f9ae9d504176
Ray <ray644302280@gmail.com>
2020-08-08 15:26:31 +1000

    fix key bug

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -11,6 +11,7 @@ const CardList = ({ robots }) => {
     return (
         robots.map(robot =>
             <Card
+                key={robot.id}
                 id={robot.id}
                 name={robot.name}
                 email={robot.email}
