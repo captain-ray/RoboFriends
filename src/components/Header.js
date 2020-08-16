4fd3c33a6dbee8f7831f70989b9a30a3f51f2de9
Ray <ray644302280@gmail.com>
2020-08-16 14:16:25 +1000

    make it pretty

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
-import React, {Component} from 'react';
-// import CounterButton from './CounterButton';
+import React, { Component } from "react";
+import CounterButton from "./CounterButton";
+import CounterButton2 from "./CounterButton2";
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
-    return false
+    return false;
   }
   render() {
-    console.log('header')
+    console.log("header");
     return (
       <div>
-        <h1 className='f1'>RoboFriends</h1>
-        {/* <CounterButton /> */}
+        <h1 className="f1">RoboFriends</h1>
+        <CounterButton />
+        <CounterButton2 />
       </div>
     );
   }
-};
+}
 
 export default Header;
